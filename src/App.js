a06c571543b111bc03e1d1580e6b85d3e8df9255
BordeiBogdan <bordei.bogdan@ivfa.ro>
2022-02-22 12:45:35 +0200

    1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,102 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import Particles from "react-tsparticles";
 
 function App() {
+  const particlesInit = (main) => {
+    console.log(main);
+
+    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+  };
+
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
+
   return (
-    <Navbar/>
+    <>
+      <Particles
+      id="tsparticles"
+      init={particlesInit}
+      loaded={particlesLoaded}
+      options={{
+        fpsLimit: 30,
+        interactivity: {
+          events: {
+            onClick: {
+              enable: true,
+              mode: "push",
+            },
+            onHover: {
+              enable: true,
+              mode: "repulse",
+            },
+            resize: true,
+          },
+          modes: {
+            bubble: {
+              distance: 400,
+              duration: 0,
+              opacity: 0.8,
+              size: 40,
+            },
+            push: {
+              quantity: 0,
+            },
+            repulse: {
+              distance: 90,
+              duration: 0.4,
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: "#ffffff",
+          },
+          links: {
+            color: "#ffffff",
+            distance: 150,
+            enable: true,
+            opacity: 0.5,
+            width: 1,
+          },
+          collisions: {
+            enable: true,
+          },
+          move: {
+            direction: "none",
+            enable: true,
+            outMode: "bounce",
+            random: true,
+            speed: 2,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 900,
+            },
+            value: 90,
+          },
+          opacity: {
+            value: 0.5,
+          },
+          shape: {
+            type: "circle",
+          },
+          size: {
+            random: true,
+            value: 10,
+          },
+        },
+        detectRetina: true,
+      }}
+      />
+      <Navbar/>
+      <Header/>
+    </>
   );
 }
 
